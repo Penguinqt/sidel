000ca58e6611aba015c960a9149a03364408644c
John Lawrence Dioquino <159016024+Penguinqt@users.noreply.github.com>
2025-10-05 19:16:19 +0800

    Initial commit: React + Springboot setup

---
+++ sidel-backend/src/main/java/com/example/sidel_backend/SidelBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.sidel_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SidelBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SidelBackendApplication.class, args);
+	}
+
+}
---
+++ sidel-backend/src/test/java/com/example/sidel_backend/SidelBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sidel_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SidelBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
